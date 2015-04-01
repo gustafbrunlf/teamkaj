16ee3290b945e49deb914e14bf4bdafaa6a6770b
gustafbrunlf <gustafbrunlof@gmail.com>
2015-04-01 12:46:48 +0000

    lagt till middleware

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -14,6 +14,7 @@ class AdminController extends Controller {
     public function __construct()
     {
         $this->middleware('auth', ['except' => [ 'index']]);
+        $this->middleware('admin', ['only' => ['index', 'create', 'store', 'edit','destroy', 'update']]);
     }
 
 	/**
