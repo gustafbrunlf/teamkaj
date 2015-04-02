56d26b6677dec5b2ce139c89070c4571dc12fdd8
gustafbrunlf <gustafbrunlof@gmail.com>
2015-04-02 12:23:26 +0000

    Ändrat rättighet för owner i prod-controller

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -19,6 +19,7 @@ class ProductsController extends Controller {
     public function __construct()
     {
         $this->middleware('auth', ['except' => ['show', 'index']]);
+        $this->middleware('admin' ['only' => ['destroy']]);
     }
 
 	/**
