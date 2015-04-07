22566681c70b5146047e478b2a3cfafde37dcd2d
jenstrom <jonatan.enstrom@gmail.com>
2015-04-07 12:02:24 +0000

    moved navbar to separate file

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,88 +10,26 @@
         @show
     </title>
 
-    <!-- Laravel CSS -->
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
-    <!-- Fonts -->
     <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
-    <!-- Our CSS -->
     <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Neuton' rel='stylesheet' type='text/css'>
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <js src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></js>
-    <js src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></js>
-    <![endif]-->
+    
 </head>
 <body>
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container-fluid">
-
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <span class="navbar-brand">Team Kaj</span>
-            </div>
-
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('products/') }}">Home</a></li>
- <li class="dropdown">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Categories<span class="caret"></span></a>
-    <ul class="dropdown-menu" role="menu">
-       
-    @foreach($categoriesmenu as $categories)
-       <li><a href="{{action('CategoriesController@show', $categories->slug)}}">{{$categories->name}}</a></li>
-    @endforeach
-
-    </ul>
-</li>
-                </ul>       
-
-
-                <ul class="nav navbar-nav navbar-right">
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/auth/login') }}">Login</a></li>
-
-                    @else
-                        <li><a href="{{ url('products/create') }}">Create new Product</a></li>
-
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                @if(Auth::user()->user_type === 0)
-                                    <li><a href="{{ action('AdminController@index') }}">Manage admins</a></li>
-                                @endif
-                                <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-
-
-    </nav>
+    
+    @include('pages._header')
 
     <div class="container" id="content">
-
         
-                @yield('body')
-            
+        @yield('body')  
 
     </div>
 
-    <!-- Scripts -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
     <script src="{{ asset('/js/script.js') }}"></script>
---
+++ resources/views/pages/_header.blade.php
@@ -0,0 +1,51 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <span class="navbar-brand">Team Kaj</span>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+            <ul class="nav navbar-nav">
+                <li><a href="{{ url('products/') }}">Home</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Categories<span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+       
+                    @foreach($categoriesmenu as $categories)
+                       <li><a href="{{action('CategoriesController@show', $categories->slug)}}">{{$categories->name}}</a></li>
+                    @endforeach
+
+                    </ul>
+                </li>
+            </ul>       
+
+            <ul class="nav navbar-nav navbar-right">
+                @if (Auth::guest())
+                    <li><a href="{{ url('/auth/login') }}">Login</a></li>
+                @else
+                    <li><a href="{{ url('products/create') }}">Create new Product</a></li>
+                    <li><a href="{{ url('categories/create') }}">Create new Category</a></li>
+
+
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            @if(Auth::user()->user_type === 0)
+                                <li><a href="{{ action('AdminController@index') }}">Manage admins</a></li>
+                            @endif
+                            <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
