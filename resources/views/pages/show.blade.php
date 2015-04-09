4b37a5b91cac2d4015d9db8b6bc759f3c064898c
Lovisa Nilsson <lo_nilsson@hotmail.com>
2015-04-09 19:02:54 +0200

    var tvungen för att få byta branch...

---
+++ resources/views/pages/show.blade.php
@@ -0,0 +1,46 @@
+@extends('master')
+
+@section('title')
+Team Kaj - {{$product->name}}
+@endsection
+
+@section('body')
+
+    <div class="row">
+        <div class="col-xs-12">
+            <h2>{{$product->name}}</h2>
+        </div>
+    </div>
+
+    <div class="row product-container single">
+        <div class="col-xs-6 single-img">
+            <img src="../{{$product->picture}}">
+        </div>
+
+        <div class="col-xs-6">
+            <div class="info">
+                <p><span class="bold">Article Number:</span> {{$product->artNo}}</p>
+                <p><span class="bold">Description:</span> {{$product->description}}</p>
+                <p><span class="bold">Stock: </span> {{$product->stock}} pcs</p>
+                <p><span class="bold">Price: </span> {{$product->price}} SEK</p>
+
+                @if($product->categories->isEmpty())
+                         <span class="label label-default">Uncategorized</span>
+                    @else 
+                        @foreach($product->categories as $categories)
+                            <a href="{{action('CategoriesController@show', $categories->slug)}}"><span class="label label-info">{{$categories->name}}</span></a>
+                        @endforeach
+                @endif
+            </div>
+        </div>
+    </div>
+
+        <br><br>
+        <a href=" {{action('ProductsController@index')}} ">Back</a>
+
+@endsection
+
+
+
+
+
