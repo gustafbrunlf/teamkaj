87536957a42bcf242841648e0101301dc50e28d2
vagrant <vagrant@homestead>
2015-04-22 16:59:00 +0000

    brought back guestmiddleware into passwordreset but made an exception for sendemail

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -32,6 +32,7 @@ class PasswordController extends Controller {
 		$this->auth = $auth;
 		$this->passwords = $passwords;
         $this->subject = 'Your Password Reset Link';
+        $this->middleware('guest',['except' => 'postEmail']);
 	}
 
 }
